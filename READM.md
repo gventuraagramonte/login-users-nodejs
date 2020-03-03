dc24f2ab34d3561608d8bd65f0454819921526de
Giorgio Ventura <gventuraagramonte@gmail.com>
2020-03-03 15:07:11 -0500

    first commit of login-node project's.

---
+++ READM.md
@@ -0,0 +1,7 @@
+## Contenido
+- La carpeta Views contiene todas las vistas HTML
+- La carpeta Routes contiene las rutas de nuestro servidor
+- La carpeta public es para nuestros archivos estaticos
+- La carpeta Models como van a lucir los datos (usuarios, nombres, correo, etc)
+- La carpeta Helpers para las funciones que podemos reutilizar en diferentes partes
+- La carpeta config es para variables de configuración
---
+++ src/index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const flash = require('connect-flash')
+
+// Initialitations
+const app = express()
+require('./database')
+
+// Settings
+app.set('port',process.env.PORT || 4000)
+app.set('views', path.join(__dirname,'views'))
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'),'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}))
+app.set('view engine', '.hbs')
+
+// Middlewares
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+app.use(session({
+    secret: 'secrectApp',
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(flash())
+
+// Globals variables
+app.use((req, res, next)=>{
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    next()
+})
+
+// Routes
+app.use(require('./routes/index'))
+app.use(require('./routes/notes'))
+app.use(require('./routes/users'))
+
+// Static files
+app.use(express.static(path.join(__dirname,'public')))
+
+// Server is listening
+app.listen(app.get('port'), ()=>{
+    console.log('Server on port: ', app.get('port'))
+})
